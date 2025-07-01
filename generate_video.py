dcd96bde7277a80d62526d3ea8f0b982cd681376
alvisjo <lwalwakk1@naver.com>
2025-07-01 10:34:25 +0900

    Supabase 통신 로그 찍기

--- generate_video.py
+++ generate_video.py
@@ -4,3 +4,8 @@ SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_API_KEY = os.getenv("SUPABASE_API_KEY")
 print(SUPABASE_URL)
 print(SUPABASE_API_KEY)
+
+
+print("HEADERS:", HEADERS)
+print("Request URL:", f"{SUPABASE_URL}/rest/v1/news?video_url=is.null&select=*")
+print("ROWS:", rows)
