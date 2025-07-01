3bfab11443c1054dfba8513b7297af5bdbe0b7e5
alvisjo <lwalwakk1@naver.com>
2025-07-01 10:36:17 +0900

    로그 수정

--- generate_video.py
+++ generate_video.py
@@ -5,7 +5,10 @@ SUPABASE_API_KEY = os.getenv("SUPABASE_API_KEY")
 print(SUPABASE_URL)
 print(SUPABASE_API_KEY)
 
+HEADERS = {
+    "apikey": SUPABASE_API_KEY,
+    "Authorization": f"Bearer {SUPABASE_API_KEY}"
+}
 
 print("HEADERS:", HEADERS)
-print("Request URL:", f"{SUPABASE_URL}/rest/v1/news?video_url=is.null&select=*")
-print("ROWS:", rows)
+print("SUPABASE_URL:", SUPABASE_URL)
