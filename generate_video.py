2e63f21a518e70036755ad964feecaca1ce1172b
alvisjo <lwalwakk1@naver.com>
2025-07-01 10:38:13 +0900

    실제 row 가져오기

--- generate_video.py
+++ generate_video.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_API_KEY = os.getenv("SUPABASE_API_KEY")
@@ -10,5 +11,10 @@ HEADERS = {
     "Authorization": f"Bearer {SUPABASE_API_KEY}"
 }
 
-print("HEADERS:", HEADERS)
-print("SUPABASE_URL:", SUPABASE_URL)
+# 뉴스 row 가져오기 테스트
+url = f"{SUPABASE_URL}/rest/v1/news?video_url=is.null&select=*"
+res = requests.get(url, headers=HEADERS)
+print("Response status:", res.status_code)
+print("Response JSON:", res.json())
+
+
